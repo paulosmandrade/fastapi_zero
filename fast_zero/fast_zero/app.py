226a7569f4a34749f25062f380984f99ea0219e8
pauloandrade1985 <paulosmandrade@hotmail.com>
2024-03-09 22:20:06 -0300

    Cadastro de Usuario

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import  UserSchema, UserPublic, UserDB
+
 app = FastAPI()
 
+database = []
 
-@app.get('/')
-def read_root():
-    return {'message': 'Olá Mundo!'}
+
+@app.post('/users', status_code=201, response_model=UserPublic)
+def create_user(user:UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database)+1)
+
+    database.append(user_with_id)
+
+    return user_with_id
