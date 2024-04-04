e6e1ec92eda8ccc84b9033410e4cad81c4d6ea41
pauloandrade1985 <paulosmandrade@hotmail.com>
2024-04-04 19:55:46 -0300

    Refatorando estrutura do projeto: Criado routers para Users e Auth

---
+++ fast_zero/fast_zero/routes/users.py
@@ -0,0 +1,81 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fast_zero.database import get_session
+from fast_zero.models import User
+from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fast_zero.security import get_current_user, get_password_hash
+
+router = APIRouter(prefix='/users', tags=['users'])
+database = []
+
+
+@router.post('/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(User.username == user.username)
+    )
+
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail='Username alredy registered'
+        )
+
+    hashed_password = get_password_hash(user.password)
+
+    db_user = User(
+        username=user.username, password=hashed_password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
+
+
+@router.get('/', status_code=200, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@router.get('/{user_id}', status_code=200, response_model=UserList)
+def read_user(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
+
+
+@router.put('/{user_id}', status_code=200, response_model=UserPublic)
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=400, detail='Not enough permissions')
+
+    current_user.username = user.username
+    current_user.password = get_password_hash(user.password)
+    current_user.email = user.email
+    session.commit()
+    session.refresh(current_user)
+
+    return current_user
+
+
+@router.delete('/{user_id}', response_model=Message)
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=400, detail='Not enough permissions')
+
+    session.delete(current_user)
+    session.commit()
+
+    return {'message': 'User deleted'}
